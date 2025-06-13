f8a7255baabc4bfc652ef9915c55855e93ea8b4b
CyberpunkIsNotDead <asuka2ndchild@gmail.com>
2025-06-14 01:13:07 +0300

    fix: parsing

--- src/lib/conversation-processing/index.ts
+++ src/lib/conversation-processing/index.ts
@@ -1,26 +1,40 @@
 import { parseCSVFile } from "./csv";
 import { MessageData } from "./csv/types";
 
+function groupMessagesByConversationId(messages: MessageData[]) {
+  const grouped: Record<string, MessageData[]> = {};
+
+  messages.forEach((message) => {
+    if (!grouped[message.conversation_id]) {
+      grouped[message.conversation_id] = [];
+    }
+
+    grouped[message.conversation_id].push(message);
+  });
+
+  return grouped;
+}
+
 async function processConversations() {
   const csvFilePath = process.env.CSV_FILE_PATH;
+
   if (!csvFilePath) {
     throw new Error("CSV_FILE_PATH environment variable is not set");
   }
 
   const messages = await parseCSVFile(csvFilePath);
 
-  const groupedByConversationId = messages.reduce(
-    (acc, message) => {
-      if (!acc[message.conversation_id]) {
-        acc[message.conversation_id] = [];
-      }
-      acc[message.conversation_id].push(message);
-      return acc;
-    },
-    {} as Record<string, MessageData[]>,
-  );
+  const groupedByConversationId = groupMessagesByConversationId(messages);
 
-  console.log(groupedByConversationId);
+  console.log(
+    "First conversation:",
+    groupedByConversationId[Object.keys(groupedByConversationId)[0]],
+  );
+  console.log("Total messages:", messages.length);
+  console.log(
+    "Total conversations:",
+    Object.keys(groupedByConversationId).length,
+  );
 
   // TODO
   // 1. Process messages
