16137fceed690d1bc14c8a07774a1e17b5bc868b
CyberpunkIsNotDead <asuka2ndchild@gmail.com>
2025-06-20 07:10:48 +0300

    fix prompt

--- src/lib/ai/prompts.ts
+++ src/lib/ai/prompts.ts
@@ -1,8 +1,8 @@
 const INSTRUCTION_PROMPT = `
+# Instructions
+
 You are a conversation processing assistant. You need to process a conversation beetwen a user (human) and an AI chatbot (bot).
-
 The conversation is given as a JSON string in the form of an array of messages.
-
 Each message has the following structure:
 {
   "role": "human" | "bot",
@@ -10,10 +10,17 @@ Each message has the following structure:
 }
 
 Your task is to process the conversation to find the conversion marks in both user and bot messages.
+
+## Conversion marks
+
 The conversion marks are the following:
 - user has chosen a service
 - user has chosen a specialist
 - user has made an appointment
+
+Conversion marks may present in different order and quantity.
+
+## Examples
 
 Example scenario for user has chosen a service:
 - user asks for a haircut
@@ -21,18 +28,46 @@ Example scenario for user has chosen a service:
 - user chooses a haircut
 
 Example scenario for user has chosen a specialist:
-- user chooses a haircut
+- user asks for a specialist
 - bot answers with available specialists
 - user chooses a specialist
 
 Example scenario for user has made an appointment:
-- user chooses a haircut and a specialist
+- user asks for an appointment
 - bot answers with available appointments
 - user chooses an appointment
 
-If bot gave user available variants and user has chosen one of them, it is a conversion mark.
+## Conversion mark conditions
 
-Return the conversion marks in the following format (messages are the messages from the conversation that triggered the conversion mark):
+Check the following conditions when you find a conversion mark.
+When checking conditions, use the conversation context.
+
+It's not a conversion mark if:
+- bot answers negatively to user's message
+- user asks for something that is not a service, specialist or appointment
+- user asks for something that is not available
+- bot gives user available variants and user doesn't choose one of them
+- user are not satisfied with bot's answer
+
+It's a conversion mark if:
+- user has chosen one of the available variants
+- user is satisfied with bot's answer
+- bot answers positively to user's message
+
+Choose specialist mark conditions:
+- there MUST be a human name
+
+Made appointment mark conditions:
+- there MUST be a date and time
+
+## Output
+
+You should return the following:
+- conversion marks
+- messages containing the conversion mark and context
+
+The output should be in the following format:
+
 [
   {
     "conversion_mark": "choose_service",
